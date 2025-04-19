810aee8a1e18e6a65af1ba8aba3011798d6d231c
Fer <fernando.falcon@holbertonstudents.com>
2025-04-19 10:28:14 -0300

    The Application Factory

--- de_blogsillo/__init__.py
+++ de_blogsillo/__init__.py
@@ -1,10 +1,25 @@
+import os
 from flask import Flask
 
-def create_app():
-  app = Flask(__name__)
+def create_app(test_config=None):
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'deBlogsillo.sqlite'),
+  )
+
+  if test_config is None:
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    app.config.from_pyfile(test_config)
+
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
 
   @app.route('/')
   def hello():
-    return '<h1>Hi there!</h1>'
+    return '<h1>Hi there! II</h1>'
   
   return app
